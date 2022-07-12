17bef7633bf96661841debe6db2605a3889866f7
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-12 16:56:02 -0300

    Criação do Projeto

---
+++ src/main/java/com/garotinho/gprojectmongo/GprojectmongoApplication.java
@@ -0,0 +1,13 @@
+package com.garotinho.gprojectmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GprojectmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GprojectmongoApplication.class, args);
+	}
+
+}
