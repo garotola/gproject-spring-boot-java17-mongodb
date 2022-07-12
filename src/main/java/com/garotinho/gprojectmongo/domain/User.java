071190f028216fe2d8e132942dd6f6683c7c0b3b
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-12 17:20:23 -0300

    Entidade de Usuário e Método Get (findAll -genérico-)

---
+++ src/main/java/com/garotinho/gprojectmongo/domain/User.java
@@ -0,0 +1,85 @@
+package com.garotinho.gprojectmongo.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    
+    private String id;
+    private String name;
+    private String email;
+
+    public User(){}
+    public User(String id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    /**
+     * @return String return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
