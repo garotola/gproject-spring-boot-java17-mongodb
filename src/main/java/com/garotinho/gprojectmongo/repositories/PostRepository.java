c9e1d41aa01dd37aede4a616cc0a4ebcae576004
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-13 15:26:23 -0300

    Consulta Simples com @Query

--- src/main/java/com/garotinho/gprojectmongo/repositories/PostRepository.java
+++ src/main/java/com/garotinho/gprojectmongo/repositories/PostRepository.java
@@ -3,11 +3,15 @@ package com.garotinho.gprojectmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.garotinho.gprojectmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+
+    @Query("{'title' : {$regex : ?0, $options : 'i'}}")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text); // Query Method que procura um post com titulo que possui o texto especificado no parametro.
 }
